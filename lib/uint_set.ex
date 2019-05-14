af3eb911ce8b63ef4c7eb8ece83588e32a1007de
Luciano Ramalho <luciano@ramalho.org>
2019-05-14 09:56:11 -0300

    first functions of UintSet tested

--- lib/uint_set.ex
+++ lib/uint_set.ex
@@ -1,5 +1,52 @@
 defmodule UintSet do
   @moduledoc """
-  Documentation for UintSet.
+  Functions that work on sets of integers >= 0.
+
+  `UintSet` is an alternative set type in Elixir
+  emulating the `MapSet` interface as closely as possible.
+  `UintSet` was created to illustrate the construction of a functional
+  data structure from scratch, using language features such as
+  protocols, streams, doctests, and unit tests.
+
+  A set can be constructed using `MapSet.new/0`:
+      iex> UintSet.new()
+      #UintSet<[]>
+
+  An `UintSet` can contain only non-negative integers.
+  By definition, sets can't contain duplicate elements:
+  when inserting an element in a set where it's already present,
+  the insertion is simply a no-op.
   """
+
+  import BitOps
+
+  defstruct bits: 0
+
+  @doc """
+  Returns a new `UintSet`.
+  ## Examples
+      iex> UintSet.new()
+      #UintSet<[]>
+  """
+  def new(), do: %UintSet{}
+
+  def new(bigint) when is_integer(bigint) and bigint >= 0 do
+    %UintSet{bits: bigint}
+  end
+
+  def to_list(%UintSet{bits: bits}) do
+    bits |> list_ones
+  end
+
+  def put(%UintSet{bits: bits}, element) do
+    %UintSet{bits: set_bit(bits, element)}
+  end
+
+  defimpl Inspect do
+    import Inspect.Algebra
+
+    def inspect(map_set, opts) do
+      concat(["#UintSet<", Inspect.List.inspect(UintSet.to_list(map_set), opts), ">"])
+    end
+  end
 end
--- test/uint_set_test.exs
+++ test/uint_set_test.exs
@@ -3,4 +3,40 @@ defmodule UintSetTest do
   doctest UintSet
 
   import UintSet
+
+  test "to_list -> []" do
+    result = UintSet.new() |> to_list()
+    assert result == []
+  end
+
+  test "to_list -> [0]" do
+    result = UintSet.new(1) |> to_list()
+    assert result == [0]
+  end
+
+  test "to_list -> [0, 1, 2, 3]" do
+    result = UintSet.new(0b1111) |> to_list()
+    assert result == [0, 1, 2, 3]
+  end
+
+  test "to_list -> [1, 100]" do
+    result =
+      (round(:math.pow(2, 100)) + 2)
+      |> UintSet.new()
+      |> to_list()
+
+    assert result == [1, 100]
+  end
+
+  test "put" do
+    [
+      {UintSet.new(), 0, [0]},
+      {UintSet.new(), 1, [1]},
+      {UintSet.new(), 1000, [1000]}
+    ]
+    |> Enum.each(fn {initial, element, wanted} ->
+      result = put(initial, element) |> to_list
+      assert result == wanted, "{#{inspect(initial)}, #{element}, #{wanted}} -> #{result}"
+    end)
+  end
 end
