8ced9b946134b91b628938a0f914203448955f4d
Luciano Ramalho <luciano@ramalho.org>
2019-05-15 20:36:39 -0300

    methods from MapSet implemented in UintSet

--- lib/uint_set.ex
+++ lib/uint_set.ex
@@ -1,22 +1,93 @@
 defmodule UintSet do
   @moduledoc """
-  Functions that work on sets of integers >= 0.
+    Functions that work on sets of integers >= 0.
 
-  `UintSet` is an alternative set type in Elixir
-  emulating the `MapSet` interface as closely as possible.
-  `UintSet` was created to illustrate the construction of a functional
-  data structure from scratch, using language features such as
-  protocols, streams, doctests, and unit tests.
+    `UintSet` is an alternative set type in Elixir
+    emulating the `MapSet` interface as closely as possible.
+    `UintSet` illustrates the construction of a functional data structure from scratch,
+    implementing the `Inspect`, `Enumerable`, and `Collectable` protocols.
 
-  A set can be constructed using `MapSet.new/0`:
-      iex> UintSet.new()
-      #UintSet<[]>
+    Many of the `UintSet` doctests and unit tests were adapted from `MapSet`.
 
-  An `UintSet` can contain only non-negative integers.
-  By definition, sets can't contain duplicate elements:
-  when inserting an element in a set where it's already present,
-  the insertion is simply a no-op.
+    A set can be constructed using `UintSet.new/0`:
+        iex> UintSet.new()
+        #UintSet<[]>
+
+    An `UintSet` can contain only non-negative integers.
+    By definition, sets can't contain duplicate elements:
+    when inserting an elem in a set where it's already present,
+    the insertion is simply a no-op.
+
+        iex> uint_set = UintSet.new()
+        #UintSet<[]>
+        iex> UintSet.put(uint_set, 3)
+        #UintSet<[3]>
+        iex> UintSet.put(uint_set, 2) |> UintSet.put(3)
+        #UintSet<[2, 3]>
+
+    `UintSet.new/1` accepts an enumerable of elements:
+
+        iex> UintSet.new(1..5)
+        #UintSet<[1, 2, 3, 4, 5]>
+
+    An `UnitSet` is represented internally using the `%UintSet{}` struct.
+    This struct can be used whenever there's a need to pattern match on something being a `MapSet`:
+        iex> match?(%UintSet{}, UintSet.new())
+        true
+
+    The `%UintSet{}` struct contains a single field—`bits`—
+    an integer which is used as a bitmap where each set bit represents
+    a number present in the set.
+
+    An empty set is represented by `bits = 0`:
+
+        iex> empty = UintSet.new()
+        iex> empty.bits
+        0
+        iex> UintSet.member?(empty, 0)
+        false
+
+    A set containing a single 0 is represented by `bits = 1`,
+    because the bit at 0 is set, so the element 0 is present:
+
+        iex> set_with_zero = UintSet.new([0])
+        iex> set_with_zero.bits
+        1
+        iex> UintSet.member?(set_with_zero, 0)
+        true
+
+    A set with a single 2 is represented by `bits = 4`,
+    because the bit at 2 is set, so the element 2 is present:
+
+        iex> set_with_two = UintSet.new([2])
+        iex> set_with_two.bits
+        4
+        iex> UintSet.member?(set_with_two, 2)
+        true
+
+    A set with the numbers 0 and 1 is represented by `bits = 3`,
+    because 3 is 0b11, so the bits 0 and 1 are set:
+
+        iex> set_with_zero_and_one = UintSet.new([0, 1])
+        #UintSet<[0, 1]>
+        iex> set_with_zero_and_one.bits
+        3
+
+    The `UintSet.new/1` function also accepts a keyword argument
+    setting the initial value of the `bits` field:
+
+        iex> UintSet.new(bits: 15)
+        #UintSet<[0, 1, 2, 3]>
+
+    This is easier to understand using base 2 notation for the argument:
+
+        iex> UintSet.new(bits: 0b1111)
+        #UintSet<[0, 1, 2, 3]>
+
+
   """
+
+  use Bitwise, only_operators: true
 
   import BitOps
 
@@ -30,26 +101,100 @@ defmodule UintSet do
   """
   def new(), do: %UintSet{}
 
-  def new(bigint) when is_integer(bigint) and bigint >= 0 do
+  def new(bits: bigint) when is_integer(bigint) and bigint >= 0 do
     %UintSet{bits: bigint}
   end
 
   def new(enumerable) do
-    uint_set = %UintSet{}
-
     enumerable
-    |> Enum.reduce(uint_set, fn item ->
-      put(uint_set, item)
-    end)
-    uint_set
+    # see Collectable protocol below
+    |> Enum.into(UintSet.new())
   end
 
   def to_list(%UintSet{bits: bits}) do
     bits |> list_ones
   end
 
-  def put(%UintSet{bits: bits}, element) do
-    %UintSet{bits: set_bit(bits, element)}
+  def put(%UintSet{bits: bits}, elem) do
+    %UintSet{bits: set_bit(bits, elem)}
+  end
+
+  @doc """
+  Deletes `value` from `uint_set`.
+  Returns a new set which is a copy of `uint_set` but without `value`.
+  ## Examples
+      iex> uint_set = UintSet.new([1, 2, 3])
+      iex> UintSet.delete(uint_set, 4)
+      #UintSet<[1, 2, 3]>
+      iex> UintSet.delete(uint_set, 2)
+      #UintSet<[1, 3]>
+  """
+  def delete(%UintSet{bits: bits}, elem) do
+    %UintSet{bits: unset_bit(bits, elem)}
+  end
+
+  def member?(%UintSet{bits: bits}, elem) do
+    get_bit(bits, elem) == 1
+  end
+
+  def equal?(%UintSet{bits: bits1}, %UintSet{bits: bits2}) do
+    bits1 == bits2
+  end
+
+  @doc """
+  Returns the number of elements in `uint_set`.
+  This function is named `length` because it needs to traverse the `uint_set`,
+  so it runs on O(n) time. The corresponding function in `MapSet` is `size`.
+
+  ## Example
+
+      iex> UintSet.length(UintSet.new([10, 20, 30]))
+      3
+  """
+  def length(%UintSet{bits: bits}) do
+    bits |> count_ones
+  end
+
+  def union(%UintSet{bits: bits1}, %UintSet{bits: bits2}) do
+    %UintSet{bits: bits1 ||| bits2}
+  end
+
+  def intersection(%UintSet{bits: bits1}, %UintSet{bits: bits2}) do
+    %UintSet{bits: bits1 &&& bits2}
+  end
+
+  @doc """
+  Returns a set that is `uint_set1` without the members of `uint_set2`.
+
+  ## Examples
+
+      iex> UintSet.difference(UintSet.new([1, 2]), UintSet.new([2, 3, 4]))
+      #UintSet<[1]>
+  """
+  def difference(%UintSet{bits: bits1}, %UintSet{bits: bits2}) do
+    %UintSet{bits: bits1 &&& bits2 ^^^ bits1}
+  end
+
+  @doc """
+  Checks if `uint_set1` and `uint_set2` have no members in common.
+
+  ## Examples
+
+      iex> UintSet.disjoint?(UintSet.new([1, 2]), UintSet.new([3, 4]))
+      true
+      iex> UintSet.disjoint?(UintSet.new([1, 2]), UintSet.new([2, 3]))
+      false
+  """
+  def disjoint?(%UintSet{bits: bits1}, %UintSet{bits: bits2}) do
+    (bits1 &&& bits2) == 0
+  end
+
+  def subset?(set1, set2) do
+    difference(set1, set2).bits == 0
+  end
+
+  def stream(%UintSet{bits: bits}) do
+    stream_ones(bits)
   end
 
   defimpl Inspect do
@@ -57,6 +202,36 @@ defmodule UintSet do
 
     def inspect(uint_set, opts) do
       concat(["#UintSet<", Inspect.List.inspect(UintSet.to_list(uint_set), opts), ">"])
+    end
+  end
+
+  defimpl Collectable do
+    def into(original) do
+      collector_fun = fn
+        set, {:cont, elem} -> UintSet.put(set, elem)
+        set, :done -> set
+        _set, :halt -> :ok
+      end
+
+      {original, collector_fun}
+    end
+  end
+
+  defimpl Enumerable do
+    def count(uint_set) do
+      {:ok, UintSet.length(uint_set)}
+    end
+
+    def member?(uint_set, val) do
+      {:ok, UintSet.member?(uint_set, val)}
+    end
+
+    def slice(_set) do
+      {:error, __MODULE__}
+    end
+
+    def reduce(uint_set, acc, fun) do
+      Enumerable.List.reduce(UintSet.to_list(uint_set), acc, fun)
     end
   end
 end
--- test/uint_set_test.exs
+++ test/uint_set_test.exs
@@ -1,34 +1,36 @@
 defmodule UintSetTest do
-  use ExUnit.Case, async: true
+  # , async: true
+  use ExUnit.Case
   doctest UintSet
 
   import UintSet
 
-  test "to_list -> []" do
+  test "to_list/1 -> []" do
     result = UintSet.new() |> to_list()
     assert result == []
   end
 
-  test "to_list -> [0]" do
-    result = UintSet.new(1) |> to_list()
+  test "to_list/1 -> [0]" do
+    result = UintSet.new(bits: 1) |> to_list()
     assert result == [0]
   end
 
-  test "to_list -> [0, 1, 2, 3]" do
-    result = UintSet.new(0b1111) |> to_list()
+  test "to_list/1 -> [0, 1, 2, 3]" do
+    result = UintSet.new(bits: 0b1111) |> to_list()
     assert result == [0, 1, 2, 3]
   end
 
-  test "to_list -> [1, 100]" do
+  test "to_list/1 -> [1, 100]" do
+    bigint = round(:math.pow(2, 100)) + 2
+
     result =
-      (round(:math.pow(2, 100)) + 2)
-      |> UintSet.new()
+      UintSet.new(bits: bigint)
       |> to_list()
 
     assert result == [1, 100]
   end
 
-  test "put" do
+  test "put/1" do
     [
       {UintSet.new(), 0, [0]},
       {UintSet.new(), 1, [1]},
@@ -36,21 +38,137 @@ defmodule UintSetTest do
     ]
     |> Enum.each(fn {initial, element, wanted} ->
       result = put(initial, element) |> to_list
-      assert result == wanted, "{#{inspect(initial)}, #{element}, #{inspect(wanted)}} -> #{inspect(result)}"
+
+      assert result == wanted,
+             "{#{inspect(initial)}, #{element}, #{inspect(wanted)}} -> #{inspect(result)}"
     end)
   end
 
-  test "new" do
+  test "into/1" do
     [
-      {UintSet.new([]), []},
-      {UintSet.new([0]), [99]},
-      {UintSet.new([1]), [1]},
-      {UintSet.new([1, 2, 3]), [1, 2, 3]},
-      {UintSet.new([1, 1000]), [1, 1000]}
+      [],
+      [0],
+      [1],
+      [1, 2, 3],
+      [1, 1000]
     ]
-    |> Enum.each(fn {initial, wanted} ->
-      result = initial |> UintSet.to_list()
-      assert result == wanted, "{#{initial.bits}, #{inspect(wanted)}} -> #{inspect(result)}"
+    |> Enum.each(fn given ->
+      result = given |> Enum.into(UintSet.new()) |> to_list
+      assert result == given, "{#{inspect(given)}} -> #{inspect(result)}"
     end)
+  end
+
+  test "member/1" do
+    refute UintSet.new() |> UintSet.member?(0)
+    assert UintSet.new([0]) |> UintSet.member?(0)
+    assert UintSet.new([1, 1000]) |> UintSet.member?(1000)
+    refute UintSet.new([1, 1000]) |> UintSet.member?(0)
+  end
+
+  # the following tests were adapted from elixir/map_set_test.exs
+
+  test "equal?/2" do
+    assert UintSet.equal?(UintSet.new(), UintSet.new())
+    refute UintSet.equal?(UintSet.new(1..20), UintSet.new(2..21))
+    assert UintSet.equal?(UintSet.new(1..120), UintSet.new(1..120))
+  end
+
+  test "union/2" do
+    result = UintSet.union(UintSet.new([1, 3, 4]), UintSet.new())
+    assert UintSet.equal?(result, UintSet.new([1, 3, 4]))
+
+    result = UintSet.union(UintSet.new(5..15), UintSet.new(10..25))
+    assert UintSet.equal?(result, UintSet.new(5..25))
+
+    result = UintSet.union(UintSet.new(1..120), UintSet.new(1..100))
+    assert UintSet.equal?(result, UintSet.new(1..120))
+  end
+
+  test "intersection/2" do
+    result = UintSet.intersection(UintSet.new(), UintSet.new(1..21))
+    assert UintSet.equal?(result, UintSet.new())
+
+    result = UintSet.intersection(UintSet.new(1..21), UintSet.new(4..24))
+    assert UintSet.equal?(result, UintSet.new(4..21))
+
+    result = UintSet.intersection(UintSet.new(2..100), UintSet.new(1..120))
+    assert UintSet.equal?(result, UintSet.new(2..100))
+  end
+
+  test "difference/2" do
+    result = UintSet.difference(UintSet.new(2..20), UintSet.new())
+    assert UintSet.equal?(result, UintSet.new(2..20))
+
+    result = UintSet.difference(UintSet.new(2..20), UintSet.new(1..21))
+    assert UintSet.equal?(result, UintSet.new())
+
+    result = UintSet.difference(UintSet.new(1..101), UintSet.new(2..100))
+    assert UintSet.equal?(result, UintSet.new([1, 101]))
+  end
+
+  test "disjoint?/2" do
+    assert UintSet.disjoint?(UintSet.new(), UintSet.new())
+    assert UintSet.disjoint?(UintSet.new(1..6), UintSet.new(8..20))
+    refute UintSet.disjoint?(UintSet.new(1..6), UintSet.new(5..15))
+    refute UintSet.disjoint?(UintSet.new(1..120), UintSet.new(1..6))
+  end
+
+  test "subset?/2" do
+    assert UintSet.subset?(UintSet.new(), UintSet.new())
+    assert UintSet.subset?(UintSet.new(1..6), UintSet.new(1..10))
+    assert UintSet.subset?(UintSet.new(1..6), UintSet.new(1..120))
+    refute UintSet.subset?(UintSet.new(1..120), UintSet.new(1..6))
+  end
+
+  test "delete/2" do
+    result = UintSet.delete(UintSet.new(), 1)
+    assert UintSet.equal?(result, UintSet.new())
+
+    result = UintSet.delete(UintSet.new(1..4), 5)
+    assert UintSet.equal?(result, UintSet.new(1..4))
+
+    result = UintSet.delete(UintSet.new(1..4), 1)
+    assert UintSet.equal?(result, UintSet.new(2..4))
+
+    result = UintSet.delete(UintSet.new(1..4), 2)
+    assert UintSet.equal?(result, UintSet.new([1, 3, 4]))
+  end
+
+  # the following test was adapted from the size/1 test in elixir/map_set_test.exs,
+  # but here the function is length/1 because it runs in O(n) time
+
+  test "length/1" do
+    assert UintSet.length(UintSet.new()) == 0
+    assert UintSet.length(UintSet.new(5..15)) == 11
+    assert UintSet.length(UintSet.new(2..100)) == 99
+  end
+
+  test "Enumerable protocol" do
+    # given, total, first, second, has_3, reversed
+    [
+      {[], 0, nil, nil, false, []},
+      {[0], 0, 0, nil, false, [0]},
+      {[1], 1, 1, nil, false, [1]},
+      {[2, 3, 4], 9, 2, 3, true, [4, 3, 2]},
+      {[3, 1000], 1003, 3, 1000, true, [1000, 3]}
+    ]
+    |> Enum.each(fn {given, total, first, second, has_3, reversed} ->
+      set = given |> UintSet.new()
+      assert total == set |> Enum.sum()
+      assert first == set |> Enum.at(0)
+      assert second == set |> Enum.at(1)
+      assert has_3 == set |> Enum.member?(3)
+      assert reversed == set |> Enum.reverse()
+    end)
+  end
+
+  test "stream/1" do
+    result =
+      UintSet.new(1..5)
+      |> UintSet.stream()
+      |> Stream.into([])
+      |> Enum.to_list()
+
+    assert result == [1, 2, 3, 4, 5]
   end
 end
