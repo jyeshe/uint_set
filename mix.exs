ea4b0c933fe78df5d396559c0fc5ec2a7300963a
Luciano Ramalho <luciano@ramalho.org>
2019-05-17 11:57:04 -0300

    preparing to publish package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule UintSet.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+
+      # Docs
+      name: "UintSet",
+      source_url: "https://github.com/ramalho/uint_set"
     ]
   end
 
@@ -21,8 +26,16 @@ defmodule UintSet.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package() do
+    [
+      licenses: ["BSD 2-Clause"],
+      links: %{"GitHub" => "https://github.com/ramalho/uint_set"}
     ]
   end
 end
